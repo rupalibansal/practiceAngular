64f1785fafd9c6fa4aedbcb17e3306f432c0b6dd
rupalibansal <r.rupali211@gmail.com>
2024-12-04 07:09:36 +1100

    made user component more generic so that it can be used by app component:@Input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import { Component, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input, signal, computed } from '@angular/core';
+// import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,15 +11,22 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-
-  // getter
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
   get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.avatar;
   }
 
+  //   selectedUser = signal(DUMMY_USERS[randomIndex]);
+  //   imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  // getter
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
